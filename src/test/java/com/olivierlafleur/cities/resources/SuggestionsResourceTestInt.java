750a8d916a1b277e35873c112d6dbafa812f70e1
Olivier Lafleur <olivier.lafleur@gmail.com>
2016-05-11 12:16:23 -0400

    Premier test

---
+++ src/test/java/com/olivierlafleur/cities/resources/SuggestionsResourceTestInt.java
@@ -0,0 +1,26 @@
+package com.olivierlafleur.cities.resources;
+
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.HttpClient;
+import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+
+import static junit.framework.Assert.assertEquals;
+
+public class SuggestionsResourceTestInt {
+    private static final String HOME_URL = "http://localhost:8080";
+    private static final String SUGGESTIONS_ENDPOINT = HOME_URL + "/suggestions";
+
+    @Test
+    public void testParameterRequired() throws IOException {
+        HttpClient httpClient = new HttpClient();
+        {
+            GetMethod getMethod = new GetMethod(SUGGESTIONS_ENDPOINT);
+            int status = httpClient.executeMethod(getMethod);
+
+            assertEquals(Response.Status.OK.getStatusCode(), status);
+        }
+    }
+}
