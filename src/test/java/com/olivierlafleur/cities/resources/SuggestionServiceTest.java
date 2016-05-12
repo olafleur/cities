5472774e3407e11f3015c16d010c5512feb865b7
Olivier Lafleur <olivier.lafleur@gmail.com>
2016-05-11 23:18:20 -0400

    Test SuggestionService

---
+++ src/test/java/com/olivierlafleur/cities/resources/SuggestionServiceTest.java
@@ -0,0 +1,35 @@
+package com.olivierlafleur.cities.resources;
+
+import com.olivierlafleur.cities.model.City;
+import com.olivierlafleur.cities.model.Suggestion;
+import com.olivierlafleur.cities.repository.Repository;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class SuggestionServiceTest {
+    @Test
+    public void queryReturnTwoSuggestions() {
+        SuggestionService suggestionService = new SuggestionService(new TestRepository());
+
+        List<Suggestion> suggestions = suggestionService.retrieveSuggestions("amos");
+
+        assertEquals(2, suggestions.size());
+    }
+
+    private class TestRepository implements Repository {
+        @Override
+        public List<City> getAllCities() {
+            List<City> cities = new ArrayList<>();
+
+            cities.add(new City(1l, "Amos", 0, 0, "Canada", "QC"));
+            cities.add(new City(2l, "Québec", 0, 0, "Canada", "QC"));
+            cities.add(new City(3l, "Omas", 0, 0, "Other country", "AB"));
+
+            return cities;
+        }
+    }
+}
