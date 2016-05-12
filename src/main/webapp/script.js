c6e79b6ee51dd898fdae83d0f29b06141e778422
Olivier Lafleur <olivier.lafleur@gmail.com>
2016-05-12 14:29:33 -0400

    AJAX call

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,4 +1,17 @@
 var citiesApp = angular.module("citiesApp", []);
-citiesApp.controller("CitiesCtrl", function($scope) {
+citiesApp.controller("CitiesCtrl", function($scope, $http) {
     $scope.name = "Calvin Hobbes";
+
+    $scope.changed = function (city) {
+        if(city.length >= 3) {
+            $http({
+                method: 'GET',
+                url: '/suggestions?q='+city
+            }).then(function successCallback(response) {
+                console.log("yé");
+            }, function errorCallback(response) {
+                console.log("nope");
+            })
+        }
+    }
 });
