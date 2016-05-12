8081c7084647564de0f56b2a75e53e54b2aca18d
Olivier Lafleur <olivier.lafleur@gmail.com>
2016-05-12 12:56:27 -0400

    Get CSV data

--- src/main/java/com/olivierlafleur/cities/repository/InMemoryRepository.java
+++ src/main/java/com/olivierlafleur/cities/repository/InMemoryRepository.java
@@ -2,6 +2,10 @@ package com.olivierlafleur.cities.repository;
 
 import com.olivierlafleur.cities.model.City;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,9 +22,30 @@ public class InMemoryRepository implements Repository {
     }
 
     private List<City> retrieveCities() {
+        return parseCsvFile("Cities.csv");
+    }
+
+    private List<City> parseCsvFile(String file) {
+        BufferedReader br;
+        String line;
         List<City> cities = new ArrayList<>();
 
-        cities.add(new City(1l, "Amos", 48.56688, -78.1162, "Canada", "QC"));
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream resourceAsStream = classLoader.getResourceAsStream(file);
+
+        try {
+            br = new BufferedReader(new InputStreamReader(resourceAsStream));
+
+            while ((line = br.readLine()) != null) {
+                String[] city = line.split(", ");
+
+                cities.add(new City(Long.valueOf(city[0]), city[1], Double.valueOf(city[2]),
+                        Double.valueOf(city[3]), city[4], city[5]));
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return cities;
     }
