aa637af239bf20b968b1d625fa0a91990f40a090
Olivier Lafleur <olivier.lafleur@vooban.com>
2016-09-13 14:55:16 -0400

    Cucumber Repository Inner class

--- src/main/java/com/olivierlafleur/cities/CucumberTestsApplication.java
+++ src/main/java/com/olivierlafleur/cities/CucumberTestsApplication.java
@@ -1,10 +1,13 @@
 package com.olivierlafleur.cities;
 
-import com.olivierlafleur.cities.repository.CucumberTestsRepository;
+import com.olivierlafleur.cities.model.City;
+import com.olivierlafleur.cities.repository.Repository;
 import com.olivierlafleur.cities.resources.SuggestionsResource;
 
 import javax.ws.rs.core.Application;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class CucumberTestsApplication extends Application {
@@ -17,5 +20,16 @@ public class CucumberTestsApplication extends Application {
     @Override
     public Set<Object> getSingletons() {
         return singletons;
+    }
+
+    private class CucumberTestsRepository implements Repository {
+        @Override
+        public List<City> getAllCities() {
+            List<City> cities = new ArrayList<>();
+
+            cities.add(new City(8L, "Leviko", 3, 4, "Canada", "Québec"));
+
+            return cities;
+        }
     }
 }
